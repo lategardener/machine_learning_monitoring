695d78a1f1f867d2ea72f1bb2670445605216383
lategardener <djolemarc@cy-tech.fr>
2026-03-15 21:38:42 +0100

    feat: ajout de service de récupération des données d'entraînement en fonction de la bibliothèque saisie

--- api-gateway/routes/model_gateway.py
+++ api-gateway/routes/model_gateway.py
@@ -2,7 +2,7 @@
 # CHARGEMENT DES BIBLIOTHÈQUES #
 # ==============================
 
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, Query
 from dependencies import verify_api_key
 import httpx
 from models.training import TrainingRequest
@@ -13,17 +13,35 @@ router = APIRouter(prefix="/models", tags=["Models Gateway"])
 
 # Url du service de redirection
 MODEL_SERVICE_URL = "http://model_service:8000/training"
+MODEL_RESULTS_URL = "http://model_SERVICE:8000/results"
 
 
-# ========================
-# ROUTE POUR REDIRECTION #
-# ========================
+# =======================================
+# ROUTE POUR L'ENTRAÎNEMENT DES MODÈLES #
+# =======================================
 
 @router.post("/training")
 async def start_training(request_data: TrainingRequest, api_key: bool = Depends(verify_api_key)):
     async with httpx.AsyncClient() as client:
         try:
             response = await client.post(MODEL_SERVICE_URL, json=request_data.model_dump())
+            response.raise_for_status()
+            return response.json()
+        except httpx.HTTPStatusError as e:
+            raise HTTPException(status_code=502, detail=f"Erreur du service : {str(e)}")
+        except httpx.RequestError as e:
+            raise HTTPException(status_code=503, detail=f"Impossible de contacter le service modèle : {str(e)}")
+
+
+# ==========================================
+# ROUTE POUR LA RÉCUPÉRATION DES RÉSULTATS #
+# ==========================================
+
+@router.get("/results")
+async def get_results(library: str = Query(..., description="Pytorch ou tensorflow"), api_key: bool = Depends(verify_api_key)):
+    async with httpx.AsyncClient() as client:
+        try:
+            response = await client.get(MODEL_RESULTS_URL, params={"library": library})
             response.raise_for_status()
             return response.json()
         except httpx.HTTPStatusError as e:
--- model-service/app/models/training.py
+++ model-service/app/models/training.py
@@ -5,10 +5,30 @@
 from pydantic import BaseModel
 
 
-# ===================
-# MODÈLE DE DONNÉES #
-# ===================
+# ==============================================
+# MODÈLE DE DONNÉES POUR LANCER L'ENTRAÎNEMENT #
+# ==============================================
 
 class TrainingRequest(BaseModel):
     dataset: str
     model_version: str
+
+
+# ================================================
+# MODÈLE DE DONNÉES POUR RÉCUPÉRER LES RÉSULTATS #
+# ================================================
+
+class TrainingResult(BaseModel):
+    run_id: str
+    library: str
+    dataset: str
+    model_name: str
+    epoch: int
+    train_loss: float
+    train_accuracy: float
+    val_loss: float
+    val_accuracy: float
+    epoch_duration: float
+    cpu_usage: float | None
+    ram_usage: float | None
+    timestamp: str
