d58db4b7b3620f1a53cb563e4e284b3756fac67a
lategardener <djolemarc@cy-tech.fr>
2026-03-15 19:21:45 +0100

    feat: Le producer n'est plus créer au début du fichier, mais de manière asynchrone pour éviter les erreurs de timeout

--- model-service/app/services/kafka_topics_producer.py
+++ model-service/app/services/kafka_topics_producer.py
@@ -4,18 +4,24 @@
 
 from kafka_utils.producer import get_producer, send_message
 from app.models.training import TrainingRequest
+import asyncio
+from functools import partial
 
+_producer = None
 
-# Récupération du producer kafka pour l'envoi des ordres d'entraînement
-producer = get_producer()
-
+def get_producer_instance():
+    global _producer
+    if _producer is None:
+        _producer = get_producer()
+    return _producer
 
 # ===================================
 # DEMANDE D'ENTRAÎNEMENT DE MODÈLES #
 # ===================================
 
-def training_order(request : TrainingRequest):
+async def training_order(request: TrainingRequest):
+    loop = asyncio.get_event_loop()
+    producer = await loop.run_in_executor(None, get_producer_instance)
     order_data = request.model_dump()
-
     send_message(producer, "training_orders", order_data)
     return {"status": "sent", "data": order_data}
