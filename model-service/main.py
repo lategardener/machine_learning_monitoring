34f35d9bc3db593d202e58e1428730ccaa0c9385
lategardener <djolemarc@cy-tech.fr>
2026-03-15 20:18:52 +0100

    feat: création de la base de données des infos issus de l'entraînement des modèles au démarrage du service modèle

--- model-service/main.py
+++ model-service/main.py
@@ -7,6 +7,8 @@ from app.routers.training_router import router as training_router
 import threading
 from app.services.kafka_metrics_consumer import listen_and_save_metrics
 from contextlib import asynccontextmanager
+from db_models.database import engine, Base
+from db_models import models
 
 
 # =================================
@@ -15,6 +17,9 @@ from contextlib import asynccontextmanager
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+
+    # Création des tables de la base de données au démarrage
+    Base.metadata.create_all(engine)
     thread = threading.Thread(target=listen_and_save_metrics, daemon=True)
     thread.start()
 
