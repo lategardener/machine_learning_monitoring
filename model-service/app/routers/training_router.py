d40b12b0ec7370608da98e562045d4191f0aa692
Sonny <berthelots@cy-tech.fr>
2026-03-29 23:11:14 +0200

    fix : log de model

--- model-service/app/routers/training_router.py
+++ model-service/app/routers/training_router.py
@@ -8,16 +8,34 @@ from app.services.kafka_topics_producer import training_order
 from app.services.training_results import get_training_results
 from typing import List
 from auth import TokenData, get_current_user
-
+from db_models.database import SessionLocal, engine
+from db_models.models import Outbox
+from sqlalchemy.orm import Session
 # Initialisation du routeur
 router = APIRouter(tags=["Training"])
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 # ===========================================
 # ROUTE POUR LE LANCEMENT DE L'ENTRAÎNEMENT #
 # ===========================================
 
 @router.post("/training")
-async def training(request: TrainingRequest, current_user: TokenData = Depends(get_current_user)):
+async def training(request: TrainingRequest, db: Session = Depends(get_db), current_user: TokenData = Depends(get_current_user)):
+    outbox_entry = Outbox(
+        event_type="model.training",
+        payload={
+            "username": current_user.username,
+            "dataset": request.dataset,
+            "model_version": request.model_version
+        }
+    )
+    db.add(outbox_entry)
+    db.commit()
     try:
         result = await training_order(request)
         return result
@@ -34,7 +52,17 @@ async def training(request: TrainingRequest, current_user: TokenData = Depends(g
 async def get_results(
         library: str = Query(..., description="Pytorch ou tensorflow"),
         dataset: str = Query(..., description="Nom du dataset"),
+        db: Session = Depends(get_db),
         current_user: TokenData = Depends(get_current_user)):
+    outbox_entry = Outbox(
+        event_type="model.get_results",
+        payload={
+            "username": current_user.username,
+            "library": library
+        }
+    )
+    db.add(outbox_entry)
+    db.commit()
     try:
         results = get_training_results(library, dataset)
         return results
