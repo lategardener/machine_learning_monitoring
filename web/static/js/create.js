f86b0ae4eef299090f6d80ab21e8a97791277c40
lategardener <djolemarc@cy-tech.fr>
2026-03-29 12:05:46 +0200

    feat: dashboard (squelette) pour la visualisation des graphes en temps réel

--- web/static/js/create.js
+++ web/static/js/create.js
@@ -1,15 +1,24 @@
+// Gestion des paramètres URL (erreur depuis redirect)
+document.addEventListener('DOMContentLoaded', () => {
+    const params = new URLSearchParams(window.location.search);
+    const errorMsg = params.get('error');
+    if (errorMsg) document.getElementById('error').innerText = errorMsg;
+});
+
 
 async function Create(e) {
     if (e) e.preventDefault();
 
-    const username = document.getElementById('username')
-    const password = document.getElementById('password')
-    const Vpassword = document.getElementById('Vpassword')
-    if (Vpassword.value !== password.value){
+    const username = document.getElementById('username');
+    const password = document.getElementById('password');
+    const Vpassword = document.getElementById('Vpassword');
+
+    if (Vpassword.value !== password.value) {
         const error = "Mot de passe différent";
         window.location.href = "/create?error=" + encodeURIComponent(error);
         return;
     }
+
     try {
         const res = await fetch('http://localhost:8000/users/createUser', {
             method: 'POST',
@@ -25,14 +34,14 @@ async function Create(e) {
         const result = await res.json();
 
         if (res.ok) {
-            success = "Compte créer avec succès, veuillez vous connecter"
-            window.location.href = "/login?success=" + encodeURIComponent(success);
+            const success = "Compte créé avec succès, veuillez vous connecter";
+            window.location.href = "/?success=" + encodeURIComponent(success);
         } else {
             const error = "Nom d'utilisateur invalide";
             window.location.href = "/create?error=" + encodeURIComponent(error);
         }
 
-    } catch (e) { 
-        window.location.href = "/create?error=Serveur indisponible";
+    } catch (e) {
+        window.location.href = "/create?error=" + encodeURIComponent("Serveur indisponible");
     }
 }
